40ddc921bb1e74a3b5672a54499eb0baa523051a
t1794116_s <t1794116_s@NEITD-123013>
2022-01-20 20:53:16 -0600

    initial version

---
+++ classwork/MovieLibrary/MovieLib.ConsoleHost/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MovieLib.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            xyz;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
